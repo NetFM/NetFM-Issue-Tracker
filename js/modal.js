0354952b98ddace7e683826cc6f008e023e3416c
Damian Savvides <dsavvides@avery.com>
2016-05-08 23:33:15 +0100

    Updated form...

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,4 @@
-document.write("<div id='signup1' class='modal'><div class='modal-content'><h4>Register your interest</h4><form action='><div class='row'><form class='col s12'><div class='row'><div class='input-field col s6'><input id='first_name' type='text' class='validate'><label for='first_name'>First Name</label></div><div class='input-field col s6'><input id='last_name' type='text' class='validate'><label for='last_name'>Last Name</label></div></div><div class='row'><div class='input-field col s12'><input id='company' type='text' class='validate'><label for='company'>Company Name</label></div></div><div class='row'><div class='input-field col s12'><input id='email' type='email' class='validate'><label for='email'>Email</label></div></div></form></div></form></div><div class='modal-footer'><a href='#' class=' modal-action modal-close waves-effect waves-green btn-flat'>Submit</a></div></div>");
+document.write("<div id='signup1' class='modal'> <div class='modal-content'> <h4>Register your interest</h4> <form action='' class='col s12'> <div class='row'> <div class='input-field col s6'> <input id='first_name' type='text' class='validate'> <label for='first_name'>First Name</label> </div> <div class='input-field col s6'> <input id='last_name' type='text' class='validate'> <label for='last_name'>Last Name</label> </div> </div> <div class='row'> <div class='input-field col s12'> <input id='company' type='text' class='validate'> <label for='company'>Company Name</label> </div> </div> <div class='row'> <div class='input-field col s12'> <input id='email' type='email' class='validate'> <label for='email'>Email</label> </div> </div> </form> </div> <div class='modal-footer'> <a href='#' class=' modal-action modal-close waves-effect waves-green btn-flat'>Submit</a> </div> </div>");
  $('.modal-trigger').leanModal({
       dismissible: true, // Modal can be dismissed by clicking outside of the modal
       opacity: .5, // Opacity of modal background
@@ -8,4 +8,40 @@ document.write("<div id='signup1' class='modal'><div class='modal-content'><h4>R
       //complete: function() { alert('Closed'); } // Callback for Modal close
     }
   );
-     
+     /*
+
+<div id='signup1' class='modal'>
+    <div class='modal-content'>
+        <h4>Register your interest</h4>
+        <form action='' class='col s12'>
+                    <div class='row'>
+                        <div class='input-field col s6'>
+                            <input id='first_name' type='text' class='validate'>
+                                <label for='first_name'>First Name</label>
+                            </div>
+                            <div class='input-field col s6'>
+                                <input id='last_name' type='text' class='validate'>
+                                    <label for='last_name'>Last Name</label>
+                                </div>
+                            </div>
+                            <div class='row'>
+                                <div class='input-field col s12'>
+                                    <input id='company' type='text' class='validate'>
+                                        <label for='company'>Company Name</label>
+                                    </div>
+                                </div>
+                                <div class='row'>
+                                    <div class='input-field col s12'>
+                                        <input id='email' type='email' class='validate'>
+                                            <label for='email'>Email</label>
+                                        </div>
+                                    </div>
+                        </form>
+                    </div>
+                    <div class='modal-footer'>
+                        <a href='#' class=' modal-action modal-close waves-effect waves-green btn-flat'>Submit</a>
+                    </div>
+                </div>
+
+
+     */
